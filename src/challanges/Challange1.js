713c7aa991ee1d5872151f1988d1c6255e7e778f
Mujee125 <mujo6999@gmail.com>
2023-12-25 16:44:54 +0500

    completed the first react project

---
+++ src/challanges/Challange1.js
@@ -0,0 +1,81 @@
+import "./style.css";
+
+const skills = [
+  {
+    skill: "HTML+CSS",
+    level: "advanced",
+    color: "#2662EA",
+  },
+  {
+    skill: "JavaScript",
+    level: "advanced",
+    color: "#EFD81D",
+  },
+  {
+    skill: "Web Design",
+    level: "advanced",
+    color: "#C3DCAF",
+  },
+  {
+    skill: "Git and GitHub",
+    level: "intermediate",
+    color: "#E84F33",
+  },
+  {
+    skill: "React",
+    level: "advanced",
+    color: "#60DAFB",
+  },
+  {
+    skill: "Svelte",
+    level: "beginner",
+    color: "#FF3B00",
+  },
+];
+
+export default function Challange1() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+
+        <div className="skill-list">
+          {skills.map((skill) => (
+            <SkillList skillObj={skill} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+function Avatar() {
+  return <img src="./jonas.jpeg" alt="Avatar" className="avatar" />;
+}
+function Intro() {
+  return (
+    <div>
+      <h1>Jonas Schmedtmann</h1>
+      <p>
+        Full-stack web developer and teacher at Udemy. When not coding or
+        preparing a course, I like to play board games, to cook (and eat), or to
+        just enjoy the Portuguese sun at beach.
+      </p>
+    </div>
+  );
+}
+function SkillList({ skillObj }) {
+  return (
+    <div className="skill" style={{ backgroundColor: skillObj.color }}>
+      <span>{skillObj.skill}</span>
+      <span>
+        {skillObj.level === "advanced" && "💪"}
+        {skillObj.level === "intermediate" && "👍"}
+        {skillObj.level === "beginner" && "👶"}
+      </span>
+    </div>
+  );
+}
